20346cd1daed13e7753e2f93e2f6bf57168af290
xbeastgameplay <160731032+xbeastgameplay@users.noreply.github.com>
2025-02-16 12:58:15 -0300

    Add files via upload

---
+++ 12/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render, HttpResponse
+
+def index(request):
+    return render(request,'index.html')
+
+def sobre(request):
+    return render(request,'sobre.html' )
+
+def contato(request):
+    return render(request,'contato.html')
+
+def ajuda(request):
+    return render(request,'ajuda.html')
